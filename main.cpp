9be35601b92c07656353ddd9f3c6f61bf2129857
MHM0321 <boxloid0321321@gmail.com>
2024-01-27 06:52:37 +0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<fstream>
+#include"raylib.h"
+using namespace std;
+int main()
+{
+	InitWindow(1500, 900, "The Legacy");
+
+	//Variables for stages
+
+
+
+
+
+	SetTargetFPS(30);
+
+	while (!WindowShouldClose())
+	{
+		//Pre game start 
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+
+
+
+
+
+}
